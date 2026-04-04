981e9ff0e29703750b1b2f4f0c1595669482d0b7
Brown-Devs <storywiseclasses@gmail.com>
2026-04-05 03:45:05 +0530

    Order Status, stats added

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -65,7 +65,7 @@ const OrderSchema = new Schema({
     },
     status: {
         type: String,
-        enum: ['new', 'delivered', 'cancelled'],
+        enum: ['new', 'prepared', 'out_for_delivery', 'delivered', 'cancelled'],
         default: 'new'
     },
     paymentStatus: {
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { authenticate } from "../middlewares/auth.middleware.js";
-import { createOrder, getOrders, updateOrder } from "../controllers/order.controller.js";
+import { createOrder, getOrders, getOrderStats, updateOrder } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
 router.post("/", authenticate, createOrder);
 router.get("/", authenticate, getOrders);
+router.get("/stats", authenticate, getOrderStats);
 router.patch("/:id", authenticate, updateOrder);
 
 export default router;
